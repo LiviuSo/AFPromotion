2c965ad50fe41da34d1db7a8a2b4f3a4bc25703e
Liviu Victor Socolovici <lsocolovici1979@gmail.com>
2016-06-10 14:50:22 -0400

    downloads the json and logs it

--- app/src/main/java/com/example/lsoco_user/app/afpromotion/PromotionListFragment.java
+++ app/src/main/java/com/example/lsoco_user/app/afpromotion/PromotionListFragment.java
@@ -1,15 +1,23 @@
 package com.example.lsoco_user.app.afpromotion;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -33,8 +41,12 @@ public class PromotionListFragment extends Fragment {
         data.add("promotion 3");
 
         // create and set the adapter
-        mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data);
+        mAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, data);
         mListView.setAdapter(mAdapter);
+
+        // download data
+        FeedDownloader downloader = new FeedDownloader();
+        downloader.execute();
 
         return view;
     }
@@ -42,11 +54,73 @@ public class PromotionListFragment extends Fragment {
     /**
      * Downloads the feed
      */
-    private class FeedDownloader {
-        private String jsonPromotions;
+    private class FeedDownloader extends AsyncTask<Void, Void, Void> {
 
-        public void downloadFeed() {
-            jsonPromotions = "";
+        private final String LOG_TAG    = FeedDownloader.class.getSimpleName();
+        private final String STRING_URL = "https://www.abercrombie.com/anf/nativeapp/Feeds/promotions.json";
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            downloadFeed();
+            return null;
+        }
+
+        private void downloadFeed() {
+            // b/e
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            // will hold the feed as json
+            String jsonPromotions;
+
+            try {
+                // create an url object
+                URL url = new URL(STRING_URL);
+                // open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // read the input stream into a string
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuilder buffer = new StringBuilder();
+
+                if (inputStream == null) {
+                    return;
+                }
+
+                // create the buffered reader
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                // read json line by line
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append('\n');
+                }
+
+                if (buffer.length() == 0) {
+                    Log.v(LOG_TAG, "buffer empty");
+                    return;
+                }
+
+                jsonPromotions = buffer.toString();
+                Log.v(LOG_TAG, jsonPromotions);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
         }
     }
 }
